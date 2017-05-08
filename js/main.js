f18abb310b65f5848c65252412abd1aa317760ca
Robbie Meyer <robbie.meyer12@gmail.com>
2017-05-08 17:04:39 -0400

    First steps to move to one page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 var height = 2.2;
 var minHeight = 2.2;
-var maxHeight = 11;
 
 function resize(){
-    if (height < maxHeight){
-        height = maxHeight;
-        document.getElementById("navbar").style.height = height + "em";
+    if (height > minHeight){
+        height = minHeight;
+        document.getElementById("navbar").style.maxHeight = height + "em";
     }
     else{
-        height = minHeight;
-        document.getElementById("navbar").style.height = height + "em";
+        height = 30;
+        document.getElementById("navbar").style.maxHeight = height + "em";
     }
 }
