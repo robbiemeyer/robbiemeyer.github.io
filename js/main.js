fe57efb978374fb8ce77bf0bdf711e56d09df960
Robbie Meyer <robbie.meyer12@gmail.com>
2017-05-08 12:00:09 -0400

    Initial Changes for 2017

--- js/main.js
+++ js/main.js
@@ -1,28 +1,14 @@
-
-
-var grow;
 var height = 2.2;
+var minHeight = 2.2;
 var maxHeight = 11;
 
 function resize(){
-	clearInterval(grow);
-	if (height < maxHeight)
-		grow = setInterval('getBig()', 5);
-	else
-		grow = setInterval('getSmall()', 5);
+    if (height < maxHeight){
+        height = maxHeight;
+        document.getElementById("navbar").style.height = height + "em";
+    }
+    else{
+        height = minHeight;
+        document.getElementById("navbar").style.height = height + "em";
+    }
 }
-function getBig(){
-	height += 0.88;
-	document.getElementById("navbar").style.height = height + 'em';
-
-	if (height >= maxHeight)
-		clearInterval(grow);
-}
-function getSmall(){
-	height -= 0.88;
-	document.getElementById("navbar").style.height = height + 'em';
-
-	if (height <= 2.2)
-		clearInterval(grow);
-}
-
